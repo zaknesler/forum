80bf56354f7bb46395516d691ba3e8d6c7ded08a
Zak Nesler <zaknes21@yahoo.com>
2016-05-20 07:09:37 -0400

    Style changes

--- resources/views/forum/topic/all.blade.php
+++ resources/views/forum/topic/all.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
 <div class="container">
-    @if ($topics->count())
-        @foreach ($topics as $topic)
-            <h1><a href="{{ route('forum.topic.show', ['id' => $topic->id]) }}">{{ $topic->title }}</a></h1>
-        @endforeach
-    @else
-        <h3>No topics to show.</h3>
-    @endif
+    <h3>All topics</h3>
+    <div class="well">
+        @if ($topics->count())
+            @foreach ($topics as $topic)
+                <h3><a href="{{ route('forum.topic.show', ['id' => $topic->id]) }}">{{ $topic->title }}</a></h3>
+            @endforeach
+        @else
+            <h3>No topics to show.</h3>
+        @endif
+    </div>
 </div>
 @endsection
--- resources/views/forum/topic/show.blade.php
+++ resources/views/forum/topic/show.blade.php
@@ -5,7 +5,8 @@
     <p><a href="{{ route('forum.topic.all') }}">Back to all topics</a></p>
 
     @if ($topic)
-        <h1>{{ $topic->title }}</h1>
+        <h1>{{ $topic->title }} <small>{{ $topic->created_at->diffForHumans() }}</small></h1>
+        <hr>
         {!! $topic->body !!}
     @endif
 </div>
