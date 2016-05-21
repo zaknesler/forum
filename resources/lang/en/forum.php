c2eb52236d1102026951570eacd84ea5cb2aaf21
Zak Nesler <zaknes21@yahoo.com>
2016-05-21 08:16:58 -0400

    some stuff

---
+++ resources/lang/en/forum.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    
+    'name' => 'Forum',
+    'welcome' => [
+        'message' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, suscipit.',
+    ],
+
+    'form' => [
+        'title' => [
+            'login' => 'Sign in',
+            'register' => 'Sign up',
+        ],
+        'submit' => [
+            'login' => 'Sign in',
+            'register' => 'Create account',
+        ],
+        'name' => [
+            'user'  => [
+                'text' => 'Username',
+            ],
+            'first' => [
+                'text' => 'First name',
+                'example' => 'Alex',
+            ],
+            'last' => [
+                'text' => 'Last name',
+                'example' => 'Smith',
+            ],
+        ],
+        'email' => [
+            'text' => 'Email',
+            'example' => 'you@domain.com',
+        ],
+        'password' => [
+            'text' => 'Password',
+            'forgot' => 'Forgot password',
+        ],
+        'remember' => [
+            'text' => 'Remember me',
+        ],
+    ],
+
+];
