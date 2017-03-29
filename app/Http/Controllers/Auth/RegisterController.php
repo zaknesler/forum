8b9b5dd0362b89e6b57ff971142317189be1754c
Zak Nesler <zaknesler@gmail.com>
2017-03-29 18:08:32 -0400

    Update name validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|min:3|max:255|unique:users',
+            'username' => 'required|min:3|max:255|alpha_dash|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
