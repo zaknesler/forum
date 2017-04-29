9ad19cc0790651873ccf47d33a5ef12fad5ad7a2
Zak Nesler <zaknes21@yahoo.com>
2017-04-29 09:01:05 -0400

    Update favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/favicon.ico" />
 
     <title>@yield('title', 'Home') &middot; {{ config('app.name') }}</title>
 
