b43de53b50394a4a382a0d47b2424f8f289ac12e
Zak Nesler <zaknesler@gmail.com>
2017-09-02 17:48:26 -0400

    Fix flash rendering

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,9 +1,11 @@
-@if (session()->has('flash_notification.message'))
+@foreach (session('flash_notification', collect())->toArray() as $message)
     <div class="flash">
         <div class="flash-wrap container">
-            <div class="alert alert-{{ session('flash_notification.level') }}">
-                {!! session('flash_notification.message') !!}
+            <div class="alert alert-{{ $message['level'] }}" role="alert">
+                {!! $message['message'] !!}
             </div>
         </div>
     </div>
-@endif
+@endforeach
+
+{{ session()->forget('flash_notification') }}
