fb65c14ac3cbf87d68390b0286965b2174d42646
Zak Nesler <zaknesler@gmail.com>
2017-11-11 19:26:16 -0500

    Fix topic list flex

--- resources/views/topics/partials/topic-list.blade.php
+++ resources/views/topics/partials/topic-list.blade.php
@@ -17,7 +17,7 @@
             </div>
         </div>
 
-        <div class="font-medium text-grey-darkest pl-4 hidden md:flex items-center" title="{{ $topic->posts->count() . ' ' . str_plural('reply', $topic->posts->count()) }}">
+        <div class="font-medium text-grey-darkest pl-4 hidden sm:flex items-center" title="{{ $topic->posts->count() . ' ' . str_plural('reply', $topic->posts->count()) }}">
             <svg class="w-4 h-4 mr-1 text-grey" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 11v3l-3-3H8a2 2 0 0 1-2-2V2c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-1zm-3 2v2a2 2 0 0 1-2 2H6l-3 3v-3H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h2v3a4 4 0 0 0 4 4h6z"/></svg>
             <span>{{ $topic->posts->count() }}</span>
         </div>
